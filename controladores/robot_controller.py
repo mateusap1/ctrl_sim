71366db804d7274f8fd4eb39b2527a89b1dc434f
Mateus Santos <mateusap1@gmail.com>
2024-06-04 08:24:49 -0300

    completed exercise 4

--- controladores/robot_controller.py
+++ controladores/robot_controller.py
@@ -19,9 +19,6 @@ class MinimalPublisher(Node):
         self.odom_subscriber = self.create_subscription(
             Odometry, "/odom", self.odom_listener_callback, 10
         )
-        self.scan_subscriber = self.create_subscription(
-            LaserScan, "/scan", self.scan_listener_callback, 10
-        )
 
         self.timer = self.create_timer(0.5, self.timer_callback)
 
@@ -38,16 +35,11 @@ class MinimalPublisher(Node):
         self.angular_errors = []
 
         # Where is my end goal?
-        self.setpoint = (-3.0, 3.0)
+        self.setpoint = (-3.0, -3.0)
 
         # This should constantly be updated by the odometry
         self.current_position = (0.0, 0.0)
         self.current_angle = 0.0
-
-        self.distance_limit = 0.5
-        self.closest_distance = math.inf
-        self.closest_angle = 0.0
-        self.left_better = False
 
         self.last_timestamp = 0.0
 
@@ -63,37 +55,6 @@ class MinimalPublisher(Node):
 
     def odom_listener_callback(self, msg: Odometry):
         self.load_odometry(msg)
-
-    def scan_listener_callback(self, msg):
-        closest_distance = math.inf
-        closest_angle = 0.0
-        closest_index = -1
-        for i, distance in enumerate(msg.ranges):
-            # angle = msg.angle_min + i * msg.angle_increment + (math.pi / 2)
-            angle = msg.angle_min + i * msg.angle_increment
-            if distance < self.closest_distance and (angle < (math.pi / 2)):
-                closest_distance = distance
-                closest_angle = angle
-
-        self.closest_distance = closest_distance
-        self.closest_angle = closest_angle
-
-        angle_range = int((math.pi / 2) / msg.angle_increment)  # 90 degrees
-        left_sum = 0
-        right_sum = 0
-        for i, distance in enumerate(msg.ranges):
-            if i < (closest_index - angle_range):
-                continue
-            if i > (closest_index + angle_range):
-                break
-
-            if distance > self.distance_limit:
-                if i < closest_index:
-                    left_sum += 1
-                else:
-                    right_sum += 1
-
-        self.left_better = left_sum > right_sum
 
     def calculate_errors(self):
         sx, sy = self.setpoint
@@ -132,9 +93,6 @@ class MinimalPublisher(Node):
 
         print("angular", aerror, angular)
 
-        if self.is_near_object():
-            self.state = "dodge_angle"
-            self.angular_errors = []
         if abs(aerror) < 0.1:
             self.state = "move_straight"
             self.angular_errors = []
@@ -160,8 +118,6 @@ class MinimalPublisher(Node):
 
         if abs(lerror) < 0.2:
             self.state = "terminal"
-        elif self.is_near_object():
-            self.state = "dodge_angle"
         elif abs(aerror) > 0.2:
             self.state = "adjust_angle"
 
@@ -174,52 +130,12 @@ class MinimalPublisher(Node):
 
         print("_________________________________________")
 
-    def dodge_angle_state(self):
-        # Makes the robot rotate until it becomes perpendicular
-        # to the closest object
-
-        print("closest angle", self.closest_angle)
-
-        angle_goal = math.pi / 2
-        if abs(self.closest_angle) >= angle_goal:
-            self.state = "dodge_straight"
-
-        msg = Twist()
-        msg.linear.x = 0.0
-        msg.angular.z = 0.25 if self.left_better else -0.25
-
-        self.velocity_publisher.publish(msg)
-        self.get_logger().info(f"Publishing {msg}")
-
-    def dodge_straight_state(self):
-        # Makes the robot move straight until it becomes far
-        # from the closes object
-
-        print("closest distance", self.closest_distance)
-
-        distance_goal = self.distance_limit * 1.2
-        if self.closest_distance > distance_goal:
-            self.state = "move_straight"
-        elif self.closest_distance <= self.distance_limit:
-            self.state = "dodge_angle"
-
-        msg = Twist()
-        msg.linear.x = 1.0
-        msg.angular.z = 0.0
-
-        self.velocity_publisher.publish(msg)
-        self.get_logger().info(f"Publishing {msg}")
-
     def timer_callback(self):
         print(self.state)
         if self.state == "adjust_angle":
             self.adjust_angle_state()
         elif self.state == "move_straight":
             self.move_straight_state()
-        elif self.state == "dodge_angle":
-            self.dodge_angle_state()
-        elif self.state == "dodge_straight":
-            self.dodge_straight_state()
         elif self.state == "terminal":
             pass
         else:
@@ -227,34 +143,9 @@ class MinimalPublisher(Node):
 
     @staticmethod
     def expected_angle(current_position: Tuple[int, int], setpoint: Tuple[int, int]):
-        distance_vertical = setpoint[0] - current_position[0]
-        distance_horizontal = setpoint[1] - current_position[1]
-
-        delta = None
-        if distance_horizontal == 0.0:
-            if distance_vertical > 0:
-                delta = math.inf
-            else:
-                delta = -math.inf
-        else:
-            delta = distance_vertical / distance_horizontal
-
-        result = abs(math.atan(delta))
-
-        if distance_vertical >= 0.0:
-            if distance_horizontal >= 0.0:
-                # Primeiro quadrante
-                return (math.pi / 2) - result
-            else:
-                # Quarto quadrante
-                return -((math.pi / 2) - result)
-        else:
-            if distance_horizontal >= 0.0:
-                # Segundo quadrante
-                return (math.pi / 2) + result
-            else:
-                # Terceiro quadrante
-                return -((math.pi / 2) + ((math.pi / 2) - result))
+        return math.atan2(
+            setpoint[1] - current_position[1], setpoint[0] - current_position[0]
+        )
 
 
 def main(args=None):
